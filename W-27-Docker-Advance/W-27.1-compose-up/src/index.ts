e9a56a85edae23d16c4c688eeba7ce6830b190be
Ali Abbas Chadhar <work.aliabbaschadhar@gmail.com>
2025-05-18 00:28:17 +0500

    new folder created and old deleted

---
+++ W-27-Docker-Advance/W-27.1-compose-up/src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.post("/", (req, res) => {
+  const { name } = req.body;
+  res.send(`Hello ${name}`);
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});

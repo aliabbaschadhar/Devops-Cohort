b37499e50975d6a15370d125529beaf21e3393ad
Ali Abbas Chadhar <work.aliabbaschadhar@gmail.com>
2025-05-17 20:58:19 +0500

    src code changed a little bit

--- W-26-Docker/W-26.2-Docker-Advance/index.js
+++ W-26-Docker/W-26.2-Docker-Advance/index.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Hey,yo bro what's up!, Example app listening on port ${port}`);
 });

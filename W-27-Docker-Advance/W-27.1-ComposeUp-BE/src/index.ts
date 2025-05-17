ad4f010fea128a8abccce38eed4fe67f99dcff94
Ali Abbas Chadhar <work.aliabbaschadhar@gmail.com>
2025-05-18 00:17:57 +0500

    express servers

--- W-27-Docker-Advance/W-27.1-ComposeUp-BE/src/index.ts
+++ W-27-Docker-Advance/W-27.1-ComposeUp-BE/src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello, world!");
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.post("/", (req: Request, res: Response) => {
+  const { name } = req.body;
+  res.send(`Hello ${name}!`);
+}
+);
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+}
+);

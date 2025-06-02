d685a42a03af651892acee3a063b9e9c70d3c1a4
Ali Abbas Chadhar <work.aliabbaschadhar@gmail.com>
2025-06-03 00:49:28 +0500

    prisma serverless-accelrate

--- todo-hono/src/index.ts
+++ todo-hono/src/index.ts
@@ -2,8 +2,36 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.post("/api/v1/signup", async (c) => {
+
+  const body = await c.req.json();
+  console.log(body);
+  console.log(c.req.header("Authorization"));
+  console.log(c.req.query("userId"))
+
+  return c.json({
+    message: "User is shaka"
+  })
+
 })
 
-export default app
+
+app.post("/api/v1/signin", (c) => {
+  return c.json({
+    message: "User is shaka"
+  })
+})
+
+app.post("/api/v1/todo", (c) => {
+  return c.json({
+    message: "User is shaka"
+  })
+})
+
+app.get("/api/v1/todo", (c) => {
+  return c.json({
+    message: "User is shaka"
+  })
+})
+
+export default app;

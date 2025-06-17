371c7d6931136e631f23859e430929afa9321e4a
Ali Abbas Chadhar <work.aliabbaschadhar@gmail.com>
2025-06-17 12:03:55 +0500

    server done

--- W-37-todo-configMaps-secrets/backend/index.ts
+++ W-37-todo-configMaps-secrets/backend/index.ts
@@ -3,5 +3,16 @@ import express from "express"
 const app = express();
 
 console.log(process.env.DATABASE_URL)
+console.log(process.env.PORT)
 
-app.listen(process.env.PORT)
+app.get("/", (req, res) => {
+  res.json({
+    db: process.env.DATABASE_URL,
+    port: process.env.PORT,
+    message: "Hello, World! This is a simple Express server."
+  })
+})
+
+app.listen(process.env.PORT, () => {
+  console.log("Server running on PORT:", process.env.PORT)
+})

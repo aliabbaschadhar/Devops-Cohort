bb0aa52e7479c0d93f2c05179aa703f060b2cfa7
Ali Abbas Chadhar <work.aliabbaschadhar@gmail.com>
2025-08-03 15:55:13 +0500

    producer done

--- W-42-Redis-Streams-Async-BEC/Producer/index.ts
+++ W-42-Redis-Streams-Async-BEC/Producer/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello via Bun!");
+import { createClient } from "redis";
+
+
+async function main() {
+  const client = await createClient()
+    .on("error", (err) => console.error("Redis client error:", err))
+    .connect()
+
+  const res = await client.xAdd("betteruptime:website", "*", {
+    url: "https://google.com",
+    id: "1"
+  })
+
+  console.log(res)
+  client.destroy() // destroying the process
+}
+
+main()
